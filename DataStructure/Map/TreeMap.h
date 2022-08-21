dbc57434ca54468975c7018f564023535e2e2e9c
lixujun <lixujun@1993.com>
2022-08-22 00:03:02 +0800

    Map未完成代码提交

--- DataStructure/Map/TreeMap.h
+++ DataStructure/Map/TreeMap.h
@@ -2,6 +2,56 @@
 
 #include "./Map.h"
 
+#define RED   false
+#define BLACK true
+
+template <typename K, typename V>
+class Node {
+public:
+    Node(K key, V value, Node<K, V> *parent) {
+        this->key = key;
+        this->value = value;
+        this->parent = parent;
+        this->left = nullptr;
+        this->right = nullptr;
+        this->color = RED;
+    }
+
+    bool isLeaf() {
+        return left == nullptr && right == nullptr;
+    }
+
+    bool hasTwoChildren() {
+        return left != nullptr && right != nullptr;
+    }
+
+    bool isLeftChild() {
+        return this->parent != nullptr && this == this->parent->left;
+    }
+
+    bool isRightChild() {
+        return this->parent != nullptr && this == this->parent->right;
+    }
+
+    // 访问兄弟节点
+    Node<K, V> *sibling() {
+        if (isLeftChild()) {
+            return parent->right;
+        } else if (isRightChild()) {
+            return parent->left;
+        } else {
+            return nullptr;
+        }
+    }
+
+    K key;
+    V value;
+    bool color;
+    Node<K, V> *left;
+    Node<K, V> *right;
+    Node<K, V> *parent;
+};
+
 template <typename K, typename V>
 class TreeMap : public Map<K, V> {
 public:
